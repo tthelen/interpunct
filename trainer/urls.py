5d8f7516db9df6153a7cef0b951c3d42e2a7fe6f
Tobias Thelen <tt@tobiasthelen.de>
2017-06-12 00:37:53 +0200

    - complete url for questionnaire redirect

--- trainer/urls.py
+++ trainer/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.task, name='task'),
-    url(r'^start$', views.start),
+    url(r'^start$', views.start, name='start'),
     url(r'^submit_task1$', views.submit_task1),
     url(r'^submit_task_correct_commas$', views.submit_task_correct_commas),
     url(r'^submit_task_explain_commas$', views.submit_task_explain_commas, name='submit_task_explain_commas'),
