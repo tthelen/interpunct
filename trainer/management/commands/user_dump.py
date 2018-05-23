55afe3f540db80db337dee40c7b9b0fb27a64c7b
Tobias Thelen <tt@tobiasthelen.de>
2018-05-23 21:18:57 +0200

    - additional manage.py command for dumping user data

---
+++ trainer/management/commands/user_dump.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from trainer.models import Rule, Sentence, SentenceRule, User
+from django.db.models import Count
+
+
+class Command(BaseCommand):
+    help = 'Debug info for user state.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('user_id', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+
+        print("Data for User {}".format(options['user_id']))
+        u = User.objects.get(user_id=options['user_id'][0])
+        s = u.get_strategy()
+        o = s.debug_output()
+        print(o)
