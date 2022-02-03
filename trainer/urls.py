f227e30cd555ccf1d8599d5abb702bfd418e901f
Tobias Thelen <tobias.thelen@uni-osnabrueck.de>
2022-02-03 10:36:49 +0100

    - Switch to Danjo >2 / Python 3.8+

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,35 +1,35 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.task, name='task'),
-    url(r'^start_new$', views.start_new, name='start_new'),
-    url(r'^start_continue$', views.start_continue, name='start_continue'),
-    url(r'^help$', views.help, name='help'),
-    url(r'^nocookies$', views.nocookies, name='nocookies'),
-    url(r'^impressum$', views.impressum, name='impressum'),
-    url(r'^code$', views.code, name='code'),
-    url(r'^logout$', views.logout_view, name='logout'),
+    path('', views.task, name='task'),
+    path('start_new', views.start_new, name='start_new'),
+    path('start_continue', views.start_continue, name='start_continue'),
+    path('help', views.help, name='help'),
+    path('nocookies', views.nocookies, name='nocookies'),
+    path('impressum', views.impressum, name='impressum'),
+    path('code', views.code, name='code'),
+    path('logout', views.logout_view, name='logout'),
 
     # ajax routes for tasks
-    url(r'^submit_task_set_commas$', views.submit_task_set_commas),
-    url(r'^submit_task_correct_commas$', views.submit_task_correct_commas),
-    url(r'^submit_task_explain_commas$', views.submit_task_explain_commas, name='submit_task_explain_commas'),
-    url(r'^delete_user$', views.delete_user),
-    url(r'^sentence/([0-9]+)$', views.sentence, name='sentence'),  # ajax load html for single correct sentence
+    path('submit_task_set_commas', views.submit_task_set_commas),
+    path('submit_task_correct_commas', views.submit_task_correct_commas),
+    path('submit_task_explain_commas', views.submit_task_explain_commas, name='submit_task_explain_commas'),
+    path('delete_user', views.delete_user),
+    path('sentence/<int:id>', views.sentence, name='sentence'),  # ajax load html for single correct sentence
 
     # statistics
-    url(r'^mystats$', views.mystats, name='mystats'),
-    url(r'^mystats_rule/$', views.mystats_rule, name='mystats_rule'),  # ajax load for single rule stat
-    url(r'^allstats_sentence/$', views.allstats_sentence, name='allstats_sentence'),
+    path('mystats', views.mystats, name='mystats'),
+    path('mystats_rule/', views.mystats_rule, name='mystats_rule'),  # ajax load for single rule stat
+    path('allstats_sentence/', views.allstats_sentence, name='allstats_sentence'),
     # ajax load for single sentence stats
-    url(r'^allstats_correct_sentence/$', views.allstats_correct_sentence, name='allstats_correct_sentence'),
+    path('allstats_correct_sentence/', views.allstats_correct_sentence, name='allstats_correct_sentence'),
     # ajax load for single sentence stats
-    url(r'^stats$', views.stats, name='stats'),
-    url(r'^ustats$', views.ustats, name='ustats'),
-    url(r'^allstats$', views.allstats, name='allstats'),  # sentence statistics for "set comma" task
+    path('stats', views.stats, name='stats'),
+    path('ustats', views.ustats, name='ustats'),
+    path('allstats', views.allstats, name='allstats'),  # sentence statistics for "set comma" task
     # sentence statistics for "correct comma" task
-    url(r'^allstats_correct$', views.allstats_correct, name='allstats_correct'),
+    path('allstats_correct', views.allstats_correct, name='allstats_correct'),
 
 ]
